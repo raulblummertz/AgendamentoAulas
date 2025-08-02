5d6d10ad5a226f3a26c0df03483a38504ed38ff6
raulblummertz <raulblummertz@gmail.com>
2025-08-02 19:42:49 -0300

    Ajuste no relatório

--- Agendamento01/Relatorios/Services/RelatorioService.cs
+++ Agendamento01/Relatorios/Services/RelatorioService.cs
@@ -15,15 +15,20 @@ namespace Agendamento01.Relatorios.Services
         }
         public async Task<RelatorioDto> RelatorioAulas(int alunoId)
         {
+            var hoje = DateTime.UtcNow;
             var aluno = await _context.Alunos.FindAsync(alunoId);
-
+            var primeiroDiaDoMes = new DateTime(hoje.Year, hoje.Month, 1, 0, 0, 0, DateTimeKind.Utc);
+            var primeiroDiaDoProximoMes = primeiroDiaDoMes.AddMonths(1);
             var aulasAgendadasMes = await _context.Agendamento
                 .CountAsync(a =>
                 a.AlunoId == alunoId &&
-                a.Aula.DataHora.Month == DateTime.Now.Month &&
-                a.Aula.DataHora.Year == DateTime.Now.Year);
+                a.Aula.DataHora >= primeiroDiaDoMes &&
+                a.Aula.DataHora < primeiroDiaDoProximoMes);
 
-            var listaAulasMaisFrequentes = await _context.Agendamento.Where(ag => ag.AlunoId == alunoId).GroupBy(a => a.Aula.TipoAula)
+            var listaAulasMaisFrequentes = await _context.Agendamento.Where(ag => ag.AlunoId == alunoId &&
+              ag.Aula.DataHora >= primeiroDiaDoMes &&
+              ag.Aula.DataHora < primeiroDiaDoProximoMes)
+                .GroupBy(a => a.Aula.TipoAula)
                 .Select(g => new FrequenciaAulasDto
                 {
                     TipoAula = g.Key,
